cf461ab33815275a1fbf9099df479f054f9c16a1
Shell1010 <amin.dev03@gmail.com>
2023-07-02 15:55:26 +0100

    Lot of fixes

--- selfbot.py
+++ selfbot.py
@@ -1,73 +1,97 @@
 import os
+
 os.system('pip uninstall selfcord')
 os.system('pip install selfcord.py')
 import selfcord
-from colorama import Fore, Back, Style
+from colorama import Back, Fore, Style
 
 prefixes = ['.', '>']
-toxxable = ['fag', 'Fag', 'kys', 'Kys', 'kill yourself', 'Kill yourself', 'kill', 'Kill', 'doxbin', 'gore', 'nigger', 'Nigger', 'niggers', 'Niggers', 'faggot', 'Faggot', 'FAggoT' 'Doxbin', 'd01ksb1n', 'dox', 'doxxing', 'doxx', 'Doxx', 'Doxxing', 'token', 'tok3n', 'give me your token', 'jewcord', 'Jewcord', 'Hitler', 'Jews', 'jews', 'hitler', 'nazi', 'Nazi', 'Heil Hitler', '1488', 'Heil Hitler', 'heil', 'Heil', 'blacks', 'Nazi Germany', 'Taiwan' , 'raid', 'raiding', 'raiders', 'rayd', 'Im raiding', 'raided', 'cp', 'Gore', 'child porn', 'pedo', 'Pedo', 'pedophile', 'Pedophile', 'nuke', 'Nuke', 'Nuking', 'nuking', 'Im nuking', 'nuking servers', 'Nuking servers', 'Nuking Servers', 'white supremacy', 'White supremacy', 'nigga', 'Nigga']
 
+toxxable = [
+    "fag",
+    "kys",
+    "kill",
+    "dox",
+    "doxbin",
+    "gore",
+    "nigger",
+    "faggot",
+    "doxxing",
+    "d01ksb1n",
+    "token",
+    "tok3n",
+    "jewcord",
+    "hitler",
+    "jews",
+    "nazi",
+    "heil",
+    "1488",
+    "blacks",
+    "taiwan",
+    "raid",
+    "raiding",
+    "rayd",
+    "cp",
+    "child porn",
+    "pedo",
+    "pedophile",
+    "nuke",
+    "nuking",
+    "white supremacy",
+    "nigga"
+]
 token = input(f"{Fore.LIGHTYELLOW_EX}[+] Token : {Style.RESET_ALL}")
 
 bot = selfcord.Bot(prefixes=prefixes)
 
 def clear():
-  try:
-    os.system('clear')
-  except:
-    os.system('cls')
+    try:
+        os.system('clear')
+    except:
+        os.system('cls')
 
 @bot.on("ready")
 async def ready(time):
-  main.clear()
-  print(f"{Fore.GREEN}{client.user} online{Style.RESET_ALL}")
-  print(f"{Fore.YELLOW}Prefix {Fore.LIGHTYELLOW_EX}{prefix}{Style.RESET_ALL}")
-  print(f"{Fore.YELLOW}Took {time} seconds to start")
-  try:
-    await client.change_presence(status=discord.Status.dnd, activity=discord.Game("ཌⱤ₲ད \\ DM To Join // ཌⱤ₲ད"))
-  except Exception as e:
-    print(e)
+    clear()
+    print(f"{Fore.GREEN}{bot.user} online{Style.RESET_ALL}")
+    print(f"{Fore.YELLOW}Prefix {Fore.LIGHTYELLOW_EX}{prefixes}{Style.RESET_ALL}")
+    print(f"{Fore.YELLOW}Took {time} seconds to start")
+    try:
+        await bot.change_presence(
+            status="dnd",
+            afk=False,
+            activity=selfcord.Activity.Game(
+                name="ཌⱤ₲ད \\ DM To Join // ཌⱤ₲ད",
+                details="DM to Join!",
+                state="vibing",
+                buttons={
+                    "Server": "https://discord.gg/9KtaxZKewk", # Change this to your liking
+                    "Wrapper": "https://pypi.org/project/selfcord.py/", # Also change this to your liking
+                },
+                application_id="1100082565811015720",
+                key="neovim",
+            ),
+        )
+    except Exception as e:
+        print(e)
 
 
 
+@bot.cmd(description="Purges your messages", aliases=['wipe'])
+async def purge(ctx, amount: int):
+    await ctx.purge(amount) # Purges amount of messages you supply
 
-@client.on("message")
+@bot.cmd(description="Spams messages")
+async def spam(ctx, amount: int, *, message: str):
+    await ctx.spam(amount, message)
+
+
+@bot.on("message")
 async def message_deleter(message):
-  if any(message.content in word for word in toxxable):
-    await message.channel.delayed_delete(message, 10)
-    print(f"[+] Deleted a message: {message.content} (REASON: Toxxable)")
-  if ".status" in message.content:
-    try:
-      command, status = message.content.split()
-      await client.change_presence(status=discord.Status.dnd, activity=discord.Game(status))
-      print("[+] Changed Status!")
-    except Exception as e:
-      print(f"[-] {e}")
-  if ".spam" in message.content:
-    try:
-      command, amount, *msg = message.content.split()
-      amount = int(amount)
-      msg = ''.join(msg)
+    if message.author == bot.user:
+        if any(word in message.content.lower() for word in toxxable):
+            await message.channel.delayed_delete(message, 10)
+            print(f"[+] Deleted a message: {message.content} (REASON: Toxxable)")
 
-      for _ in range(amount):
-        try:
-          await message.channel.send(msg)
-        except:
-          pass
-    except:
-      pass
-  if ".purge" in message.content:
-    try:
-      messages = await message.channel.history(limit=100).flatten()
-      for msg in messages:
-        if message.author == client.user:
-          try:
-            await msg.delete()
-          except:
-            pass
-    except Exception as e:
-      print(f"[-] {e}")
-  else:
-    pass
 
-client.run(token, bot=False)
+bot.run(token)
